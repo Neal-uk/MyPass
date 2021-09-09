4197e1f3d3ec1ddf9b6a02d71f2e90788bb116ba
Neal-uk <87118071+Neal-uk@users.noreply.github.com>
2021-09-09 12:49:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from tkinter import * # import all Classes and constants, though not messagebox 
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-#Password Generator Project **DAY 5**
+
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -39,6 +40,7 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password) # password gets copied to clipboard automatically
     print(f"Your password is: {password}")
+    
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save():
@@ -58,14 +60,6 @@ def save():
                 data_file.write(f"{website} | {email} | {password}\n")
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
-
-
-
-
-
-
-
-
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -97,7 +91,6 @@ email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "email@hotmail.com")
 password_entry = Entry(width=32)
 password_entry.grid(row=3, column=1)
-
 
 #Buttons
 generate_password_button = Button(text="Generate Password", command=generate_password)
